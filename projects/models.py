4a0846034758bfa84c154e503e7c9a62ee5494f0
Rich Raffals <rich@awakenthemagic.com>
2020-07-07 18:06:27 -0700

    Add superuser rich + changes to activate admin area features (provides UI to modify DBs)

--- projects/models.py
+++ projects/models.py
@@ -8,3 +8,8 @@ class Project(models.Model):
     technology = models.CharField(max_length=300)
     image = models.CharField(max_length=100)
     project_link = models.URLField()
+
+    # Print project title in admin interface (doensn't work)
+    def __str__(self):
+        return self.title  # or any of the other appropriate variables
+    

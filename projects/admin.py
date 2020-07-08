fba4c9494d528431901f584c89cee000e41742e6
Rich Raffals <rich@awakenthemagic.com>
2020-07-07 18:06:27 -0700

    Changes to activate admin area features (provides UI to modify DBs) + add superuser

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from projects.models import Project
 
 # Register your models here.
+admin.site.register(Project)
+
+# Admin area customizations
+admin.site.site_header = "Portfolio Admin"
+admin.site.site_title = "Snazzy Site Title"
+admin.site.index_title = "Index Title"
